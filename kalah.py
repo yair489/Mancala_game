2bbf93bc53285ceaae96ab518e78e1010e3948f0
yair489 <yairturgeman@gmail.com>
2025-02-18 19:19:11 +0200

    init API

--- kalah.py
+++ kalah.py
@@ -1,2 +1,22 @@
 class Kalah:
-    pass
+    def __init__(self , holes , seedes):
+        self.seedes = seedes
+        self.holes = holes
+        self.kalah = [0]*(holes*2+2)
+        self.kalah[6] = self.kalah[13] = 0
+
+
+    def  play(self , hole):
+        if hole<0 or hole > 11:
+            raise IndexError
+        # if self.seedes == 0 raise Value erir
+        return "Tie"
+
+    def status(self):
+        return tuple(self.kalah)
+
+    def done(self):
+        return True
+
+    def score(self):
+        return [0,0]
