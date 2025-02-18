1b4520bef4d294b65bfd868b3446fd14b28b0752
yair489 <yairturgeman@gmail.com>
2025-02-18 19:36:19 +0200

    1.1 inital status

--- test_kalah.py
+++ test_kalah.py
@@ -1,6 +1,13 @@
+import pytest
+
 import kalah
+import py
 
 
-def test_create_kalah_instance():
+@pytest.fixture
+def game():
+    return kalah.Kalah(6 , 4)
+def test_create_kalah_instance(game):
     """This is an example test. Please delete me."""
-    game = kalah.Kalah()
+    # game = kalah.Kalah()
+    assert game.status() == (4,4,4,4,4,4,0,4,4,4,4,4,4,0)
