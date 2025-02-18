bf60425974fd80467fd055397b0072074bfadc71
yair489 <yairturgeman@gmail.com>
2025-02-18 19:43:18 +0200

    fix 1.1 illegal hole

--- test_kalah.py
+++ test_kalah.py
@@ -11,3 +11,12 @@ def test_create_kalah_instance(game):
     """This is an example test. Please delete me."""
     # game = kalah.Kalah()
     assert game.status() == (4,4,4,4,4,4,0,4,4,4,4,4,4,0)
+
+
+def test_illegal_hole(game):
+    assert pytest.raises( IndexError, game.play ,  -1)
+    assert pytest.raises(IndexError, game.play, 20)
+    assert pytest.raises(IndexError, game.play, -2)
+    assert pytest.raises(IndexError, game.play, -11)
+    assert pytest.raises(IndexError, game.play, -12)
+    assert pytest.raises(IndexError, game.play, 100)
